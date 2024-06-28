fd337504bbfd4b856b3507cda796879259bee1b3
Zhijie Fang <847749593@qq.com>
2024-06-28 12:04:16 +0800

    doc

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,8 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '口袋精灵2 | 单机版 不完全指南',
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme()
 })
